836fa746e8d123cad52594f8356a7838ce697b4d
S1NJED <oooguuh@gmail.com>
2025-12-06 17:31:03 +0100

    rename to agent.py
    Signed-off-by: S1NJED <oooguuh@gmail.com>

--- src/agents/agent.py
+++ src/agents/agent.py
@@ -13,9 +13,17 @@ from typing import Dict, Any
 def query_meilisearch(text_input: str):
     url = "https://edge.meilisearch.com/multi-search"
     payload = {"queries":[{"indexUid":"movies-en-US","q":text_input,"attributesToHighlight":["*"],"highlightPreTag":"__ais-highlight__","highlightPostTag":"__/ais-highlight__","limit":9,"offset":0,"hybrid":{"embedder":"small","semanticRatio":0.7},"rankingScoreThreshold":0.2}]}
-    
-    req = requests.post(url, data=payload)
+    headers = {
+        "Authorization": "Bearer 6287312fd043d3fca95136cd40483a26154d37dc99aa2e79417f88794a80cd1c",
+        "Content-length": str(len(payload)),
+        "Content-type": "application/json"
+    }
+
+    req = requests.post(url, headers=headers, json=payload)
+    if req.status_code != 200:
+        return "Request failed with status code: " + str(req.status_code)
     data: MeilisearchMultiResponse = req.json()
+    return data
     
 class AdvancedAgent:
     def __init__(self, api_key: str, model_name: str = "gemini-2.5-flash"):
@@ -83,6 +91,7 @@ class AdvancedAgent:
 
 # --- Running the Agent (Remains the same) ---
 if __name__ == "__main__":
+
     load_dotenv()
     API_KEY = os.getenv("GOOGLE_AI_API_KEY") 
     
